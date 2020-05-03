3b587f3b4e11690e44e242220f687aace36eaa9f
Marcelo Cabral Ghilardi <kabrau@gmail.com>
2020-05-03 03:19:14 -0300

    WebSite first template

--- server/equipe28App/routes/index.js
+++ server/equipe28App/routes/index.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
-    
-  var today =  new Date()
+  res.redirect("/home/")
 
-  res.render('index', { 
-    title: 'Express 1.10 '+today
-  });
+  //var x = path.join(__dirname,'/../public/template01/index.html');
+  //var x = path.join('../template/index.html');
+  //console.log(x)
+  //res.sendFile(x);
+    
+  //var today =  new Date()
+  // res.render('index', { 
+  //   title: 'Express 1.10 '+today
+  // });
 });
 
 module.exports = router;
